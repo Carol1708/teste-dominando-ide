e783c0718c001c572292369478e134e969196a68
Carol1708 <carolqueiroz96@outlook.com.br>
2022-01-20 13:19:00 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Livro livro = new Livro();
+		Gato gato = new Gato();	
+		
+	System.out.println(gato);
+	System.out.println(livro);
+	
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello world!" + (a+b));*/
+
+	}
+
+}
---
+++ src/br/com/dio/model/Livro.java
@@ -0,0 +1,8 @@
+package br.com.dio.model;
+
+public class Livro {
+	
+	private String nome;
+	private String npag;
+
+}
